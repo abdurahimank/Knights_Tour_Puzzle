78f8c99de45dd4efe9ed67c9c1af563d36d73505
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-10 15:22:40 +0530

    Stage 2/6

--- stages.py
+++ stages.py
@@ -1,32 +1,45 @@
-# Stage 1/6: Setting up the board
+# Stage 2/6: And now for something completely different!
 class KnightTourPuzzle:
     def __init__(self):
-        self.x = [["_" for _ in range(8)] for _ in range(8)]
+        self.board = []
+        self.row, self.col, self.cell_size = 0, 0, 0
 
     def display(self):
-        print(f""" -------------------
-8| {self.x[7][0]} {self.x[7][1]} {self.x[7][2]} {self.x[7][3]} {self.x[7][4]} {self.x[7][5]} {self.x[7][6]} {self.x[7][7]} |
-7| {self.x[6][0]} {self.x[6][1]} {self.x[6][2]} {self.x[6][3]} {self.x[6][4]} {self.x[6][5]} {self.x[6][6]} {self.x[6][7]} |
-6| {self.x[5][0]} {self.x[5][1]} {self.x[5][2]} {self.x[5][3]} {self.x[5][4]} {self.x[5][5]} {self.x[5][6]} {self.x[5][7]} |
-5| {self.x[4][0]} {self.x[4][1]} {self.x[4][2]} {self.x[4][3]} {self.x[4][4]} {self.x[4][5]} {self.x[4][6]} {self.x[4][7]} |
-4| {self.x[3][0]} {self.x[3][1]} {self.x[3][2]} {self.x[3][3]} {self.x[3][4]} {self.x[3][5]} {self.x[3][6]} {self.x[3][7]} |
-3| {self.x[2][0]} {self.x[2][1]} {self.x[2][2]} {self.x[2][3]} {self.x[2][4]} {self.x[2][5]} {self.x[2][6]} {self.x[2][7]} |
-2| {self.x[1][0]} {self.x[1][1]} {self.x[1][2]} {self.x[1][3]} {self.x[1][4]} {self.x[1][5]} {self.x[1][6]} {self.x[1][7]} |
-1| {self.x[0][0]} {self.x[0][1]} {self.x[0][2]} {self.x[0][3]} {self.x[0][4]} {self.x[0][5]} {self.x[0][6]} {self.x[0][7]} |
- -------------------
-   1 2 3 4 5 6 7 8 """)
+        print(" " * len(str(self.row)) + "-" * (self.col * (self.cell_size + 1) + 3))
+        for i in range(self.row, 0, -1):
+            print((" " * (len(str(self.row)) - len(str(i)))) + str(i) + "| " + " ".join(self.board[i - 1]) + " |")
+        print(" " * len(str(self.row)) + "-" * (self.col * (self.cell_size + 1) + 3))
+        temp = [" " * (self.cell_size - len(str(i))) + str(i) for i in range(1, self.col + 1)]
+        print(" " * (len(str(self.row)) + 2) + " ".join(temp) + "  ")
+
+    def create_board(self):
+        while True:
+            try:
+                self.col, self.row = [int(i) for i in input("Enter your board dimensions: ").split()]
+                if self.row <= 0 or self.col <= 0:
+                    raise ValueError
+                else:
+                    self.cell_size = len(str(self.col * self.row))
+                    self.board = [["_" * self.cell_size for _ in range(self.col)] for _ in range(self.row)]
+                    break
+            except ValueError:
+                print("Invalid dimensions!")
 
     def start(self):
-        try:
-            y, x = [int(i) for i in input("Enter the knight's starting position: ").split()]
-            if 1 <= x <= 8 and 1 <= y <= 8:
-                self.x[x - 1][y - 1] = "X"
-                self.display()
-            else:
-                print("Invalid dimensions!")
-        except ValueError:
-            print("Invalid dimensions!")
+        self.create_board()
+        while True:
+            try:
+                y, x = [int(i) for i in input("Enter the knight's starting position: ").split()]
+                if 1 <= x <= self.row and 1 <= y <= self.col:
+                    self.board[x - 1][y - 1] = " " * (self.cell_size - 1) + "X"
+                    self.display()
+                    break
+                else:
+                    print("Invalid position!")
+            except ValueError:
+                print("Invalid position!")
 
 
 knight_tour = KnightTourPuzzle()
 knight_tour.start()
+
