ca1ac4c8fb924f57890a4fdca8ecd4d2f0e93b83
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-22 16:11:33 +0530

    Stage 5.5/6

--- stages.py
+++ stages.py
@@ -1,16 +1,8 @@
-# Stage 5/6: How far will your knight go?
+# Stage 5.5/6: Automatic answer
 class KnightTourPuzzle:
     def __init__(self):
-        self.board, self.valid_moves = [], []
-        self.x, self.y, self.row, self.col, self.cell_size, self.visited = 0, 0, 0, 0, 0, 0
-
-    def display(self):
-        print(" " * len(str(self.row)) + "-" * (self.col * (self.cell_size + 1) + 3))
-        for i in range(self.row, 0, -1):
-            print((" " * (len(str(self.row)) - len(str(i)))) + str(i) + "| " + " ".join(self.board[i - 1]) + " |")
-        print(" " * len(str(self.row)) + "-" * (self.col * (self.cell_size + 1) + 3))
-        temp = [" " * (self.cell_size - len(str(i))) + str(i) for i in range(1, self.col + 1)]
-        print(" " * (len(str(self.row)) + 2) + " ".join(temp) + "  " + "\n")
+        self.board, self.solution = [], []
+        self.x, self.y, self.row, self.col, self.cell_size, self.count = 0, 0, 0, 0, 0, 1
 
     def create_board(self):
         while True:
@@ -25,7 +17,7 @@ class KnightTourPuzzle:
             except ValueError:
                 print("Invalid dimensions!")
 
-    def start_game(self):
+    def starting_position(self):
         while True:
             try:
                 self.y, self.x = [int(i) for i in input("Enter the knight's starting position: ").split()]
@@ -33,12 +25,20 @@ class KnightTourPuzzle:
                     self.board[self.x - 1][self.y - 1] = " " * (self.cell_size - 1) + "X"
                     break
                 else:
-                    print("Invalid position!")
+                    raise ValueError
             except ValueError:
                 print("Invalid position!")
 
-    def game_status(self):
-        if self.visited == self.row * self.col:
+    def display(self):
+        print(" " * len(str(self.row)) + "-" * (self.col * (self.cell_size + 1) + 3))
+        for i in range(self.row, 0, -1):
+            print((" " * (len(str(self.row)) - len(str(i)))) + str(i) + "| " + " ".join(self.board[i - 1]) + " |")
+        print(" " * len(str(self.row)) + "-" * (self.col * (self.cell_size + 1) + 3))
+        temp = [" " * (self.cell_size - len(str(i))) + str(i) for i in range(1, self.col + 1)]
+        print(" " * (len(str(self.row)) + 2) + " ".join(temp) + "  " + "\n")
+
+    def game_status(self, board):
+        if not any([1 if j == "_" * self.cell_size else 0 for i in board for j in i]):
             return "won"
         elif self.valid_move(self.x, self.y) == 0:
             return "lost"
@@ -46,56 +46,43 @@ class KnightTourPuzzle:
 
     def valid_move(self, x, y, option="count"):
         valid_move = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, 2), (1, -2), (2, -1), (2, 1)]
-        total_move = 0
-        self.valid_moves = []
+        total_move, valid_moves = 0, {}
         for i in valid_move:
             if 1 <= x + i[0] <= self.row and 1 <= y + i[1] <= self.col \
                     and self.board[x + i[0] - 1][y + i[1] - 1] == "_" * self.cell_size:
                 total_move += 1
-                self.valid_moves.append((x + i[0], y + i[1]))
                 if option == "replace":
                     self.board[x + i[0] - 1][y + i[1] - 1] = " " * (self.cell_size - 1) \
                                                              + str(self.valid_move(x + i[0], y + i[1]))
-        return total_move
+                elif option == "rank":
+                    valid_moves[(x + i[0], y + i[1])] = self.valid_move(x + i[0], y + i[1])
+        return total_move if option == "count" else valid_moves
 
-    def next_move(self):
+    def check_solution(self):
+        self.solution = self.board
         while True:
-            try:
-                self.y, self.x = [int(i) for i in input("Enter your next move: ").split()]
-                if 1 <= self.x <= self.row and 1 <= self.y <= self.col and self.board[self.x - 1][self.y - 1] \
-                        not in [" " * (self.cell_size - 1) + "*", " " * (self.cell_size - 1) + "X"] \
-                        and (self.x, self. y) in self.valid_moves:
-                    self.board[self.x - 1][self.y - 1] = " " * (self.cell_size - 1) + "X"
-                    self.valid_move(self.x, self.y, "replace")
-                    break
-                else:
-                    print("Invalid move!")
-            except ValueError:
-                print("Invalid move!")
+            if self.game_status(self.solution) == "continue":
+                self.solution[self.x - 1][self.y - 1] = " " * (self.cell_size - len(str(self.count))) + str(self.count)
+                rank_dic = sorted(self.valid_move(self.x, self.y, "rank").items(), key=lambda x: x[1])
+                if len(rank_dic) > 1:
+                    rank_dic = [i for i in rank_dic if i[1] > 0 and any([i[1] for i in rank_dic])]
+                if self.count < self.row * self.col:
+                    self.x, self.y = rank_dic[0][0]
+                self.count += 1
+            elif self.game_status(self.solution) == "won":
+                return True
+            else:
+                self.solution[self.x - 1][self.y - 1] = " " * (self.cell_size - len(str(self.count))) + str(self.count)
+                return False
 
     def play(self):
         self.create_board()
-        self.start_game()
-        while True:
-            self.valid_move(self.x, self.y, "replace")
-            self.display()
-            self.board[self.x - 1][self.y - 1] = " " * (self.cell_size - 1) + "*"
-            for i in range(self.row):
-                for j in range(self.col):
-                    if self.board[i][j] != " " * (self.cell_size - 1) + "*":
-                        self.board[i][j] = "_" * self.cell_size
-
-            self.visited = sum([0 if j == "_" * self.cell_size else 1 for i in self.board for j in i])
-            if self.game_status() == "continue":
-                self.next_move()
-            elif self.game_status() == "won":
-                print("What a great tour! Congratulations!")
-                break
-            elif self.game_status() == "lost":
-                print("No more possible moves!")
-                print(f"Your knight visited {self.visited} squares!")
-                break
+        self.starting_position()
+        self.check_solution()
+        self.board = self.solution
+        self.display()
 
 
 knight_tour = KnightTourPuzzle()
 knight_tour.play()
+
